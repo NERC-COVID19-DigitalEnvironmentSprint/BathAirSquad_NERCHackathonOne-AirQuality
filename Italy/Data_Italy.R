24f363a62c6ed92ed6bd5ca5718dc41e980c7c9b
lcol20 <66132361+lcol20@users.noreply.github.com>
2020-06-04 09:51:40 +0100

    Add files via upload

--- Italy/Data_Italy.R
+++ Italy/Data_Italy.R
@@ -3,13 +3,13 @@ library(lubridate)
 library(dplyr)
 library(readr)
 
-setwd("C:/Users/lauri/Downloads/Challenge1/Italy")
+setwd("C:/Users/lauri/Downloads/Challenge1/Italy/Italy")
 
 # Daily pollutant levels(NO2,PM2.5,PM10) in ug/m3 by province, 2018 - 2020
 # https://www.eea.europa.eu/themes/air/air-quality-and-covid19/monitoring-covid-19-impacts-on
 
-NO2_1820 = read_csv('NO2_1820.csv')
-NO2 = NO2_1820 %>% select(AirPollutant, AirQualityLevel, LatitudeOfMeasurementStation, 
+NO2_1820 <- read_csv('NO2_1820.csv')
+NO2 <- NO2_1820 %>% select(AirPollutant, AirQualityLevel, LatitudeOfMeasurementStation, 
                           LongitudeOfMeasurementStation, City, UA_city_pop, datebegin) %>% rename(
                             pollutant = AirPollutant,
                             NO2_val= AirQualityLevel,
@@ -20,8 +20,8 @@ NO2 = NO2_1820 %>% select(AirPollutant, AirQualityLevel, LatitudeOfMeasurementSt
                             date = datebegin
                           )
 
-PM2.5_1820 = read_csv('PM2.5_1820.csv')
-PM2.5 = PM2.5_1820 %>% select(AirPollutant, AirQualityLevel, LatitudeOfMeasurementStation, 
+PM2.5_1820 <- read_csv('PM2.5_1820.csv')
+PM2.5 <- PM2.5_1820 %>% select(AirPollutant, AirQualityLevel, LatitudeOfMeasurementStation, 
                               LongitudeOfMeasurementStation, City, UA_city_pop, datebegin) %>% rename(
                                 pollutant = AirPollutant,
                                 PM2.5_val= AirQualityLevel,
@@ -32,8 +32,8 @@ PM2.5 = PM2.5_1820 %>% select(AirPollutant, AirQualityLevel, LatitudeOfMeasureme
                                 date = datebegin
                               )
 
-PM10_1820 = read_csv('PM10_1820.csv')
-PM10 = PM10_1820 %>% select(AirPollutant, AirQualityLevel, LatitudeOfMeasurementStation, 
+PM10_1820 <- read_csv('PM10_1820.csv')
+PM10 <- PM10_1820 %>% select(AirPollutant, AirQualityLevel, LatitudeOfMeasurementStation, 
                             LongitudeOfMeasurementStation, City, UA_city_pop, datebegin) %>% rename(
                               pollutant = AirPollutant,
                               PM10_val= AirQualityLevel,
@@ -45,6 +45,11 @@ PM10 = PM10_1820 %>% select(AirPollutant, AirQualityLevel, LatitudeOfMeasurement
                               
                             )
 
+cityandregion <- read_csv('cityandregion.csv')
+NO2 <- left_join(NO2, cityandregion, by = c('city'='city'))
+PM2.5 <- left_join(PM2.5, cityandregion, by = c('city'='city'))
+PM10 <- left_join(PM10, cityandregion, by = c('city'='city'))
+
 
 # Data on COVID-19 in Italy included total regional and provincial confirmed cases, regional number 
 # of performed and positive swab test, regional number of fatal events ascribed to SARS-CoV-2.
@@ -52,9 +57,9 @@ PM10 = PM10_1820 %>% select(AirPollutant, AirQualityLevel, LatitudeOfMeasurement
 # https://github.com/pcm-dpc/COVID-19	
 
 #covid by region
-covid_link = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv"
-covidbyreg = read_csv(covid_link) 
-covidbyregion = covidbyreg %>% select(-stato ,-casi_testati, -note_it, -note_en) %>% rename(
+covid_link <- "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv"
+covidbyreg <- read_csv(covid_link) 
+covidbyregion <- covidbyreg %>% select(-stato ,-casi_testati, -note_it, -note_en) %>% rename(
                                        date = data,
                                        region_code= codice_regione,
                                        region= denominazione_regione,
@@ -76,8 +81,16 @@ covidbyregion = covidbyreg %>% select(-stato ,-casi_testati, -note_it, -note_en)
 #Table with area, population and population density. Elaborated from: 
 #Area per region: http://dati.istat.it/Index.aspx?QueryId=37449#
 #Population 01/01/19 (most recent): http://dati.istat.it/Index.aspx?QueryId=18460
-areabyregion = read_csv('areabyregion.csv')
+info_by_region <- read_csv('info_by_region.csv')
 
-#shp
-shp_link = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/aree/shp/dpc-covid-19-ita-aree.shp"
-shp = st_read(shp_link)
+meancovid_region <- aggregate(covidbyregion, list(covidbyregion$region), mean)
+meancovid_region <- select(meancovid_region, -region_code, -region) %>% rename(region = Group.1)
+
+covid_It_byregion <- left_join(meancovid_region, info_by_region, by = c('region'='region'))
+
+#regions of Italy .geojson
+link= 'https://gist.github.com/datajournalism-it/f1abb68e718b54f6a0fe'
+mymap <- st_read("regioni-con-trento-bolzano.geojson")
+mymap$Regione <- info_by_region$region
+plot(mymap)
+
